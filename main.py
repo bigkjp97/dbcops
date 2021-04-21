e3f6931d94fcfc6924b9c2293344be1790ec59af
bigkjp97 <kunzhoupeng@gmail.com>
2021-04-21 17:01:47 +0800

    fixed multiple threads problem

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 import yaml
 import argparse
 
+from cx_Oracle import init_oracle_client
 from prometheus_client import start_http_server
 
 from utils.oracleSql import oracleSQL
@@ -9,27 +10,20 @@ from utils.oracleSql import oracleSQL
 
 def main():
     parser = argparse.ArgumentParser()
-    # 设置传参方式
+    # parameter from system
     parser.add_argument('-f', '--file', default='./init.yml')
     args = parser.parse_args()
     with open(args.file, 'r') as conf:
         c = yaml.load(conf, Loader=yaml.FullLoader)
 
     # print(configSet)
+    init_oracle_client(
+        lib_dir=c['server']['instantclient'])
     start_http_server(int(c['server']['port']), c['server']['host'])
-    client = c['server']['instantclient']
     for config in c['config']:
-        o = threading.Thread(target=oracleSQL(config, client).do_execute())
+        # print(config)
+        o = threading.Thread(target=oracleSQL(config).do_execute)
         o.start()
-    # for case in cases:
-    #     file = case['file']
-    #     job = case['job']
-    #     keywords = case['keywords']
-    #     label = case['label']
-    #     # 开启多线程
-    #     t = threading.Thread(target=tail.Tail(file, job, keywords, label, pushHost,hostName).tail)
-    #     t.start()
-    #     # threads.append(t)
 
 
 if __name__ == '__main__':
--- utils/oracleSql.py
+++ utils/oracleSql.py
@@ -6,51 +6,50 @@ import cx_Oracle
 
 
 class oracleSQL:
-    def __init__(self, config, client):
+    def __init__(self, config):
         self._database = config['database']
         self._sqlText = config['sqltext']
         self._matric = config['matric']
         self._tag = config['tag']
         self._label = config['label']
         self._interval = config['interval']
-        self._client = client
         self._gauge = Gauge('dbcops_' + self._tag + '_' + self._matric, 'Return value of column ' + self._matric,
                             self._label)
 
     def fetch_table(self, cursor, sql):
-        # 下标计数
+        # count index
+        global valIndex
         index = 0
         labIndex = []
         cursor.execute(sql)
         # (name, type, display_size, internal_size, precision, scale, null_ok)
-        # 读取每列的数据类型及名称
+        # read every column
         for column in cursor.description:
-            # 符合varchar且不能为空的值作为label
+            # VARCHAR and NOT NULL
             if column[0] in self._label:
                 if column[6] != 0 and (column[1] != cx_Oracle.DB_TYPE_VARCHAR or column[1] != cx_Oracle.DB_TYPE_CHAR):
                     raise KeyError("The label '" + column[0] + "' is not suitable, make sure it's not null and char")
                 labIndex.append(index)
-            # 符合number类型即为需要采集的数值
+            # collect NUMBER data
             if column[0] == self._matric:
                 if column[1] != cx_Oracle.DB_TYPE_NUMBER:
                     raise ValueError("Value of this matric is not NUMBER")
-            # 下标右移
+                # NUMBER column's index
+                valIndex = index
             index += 1
-        # numColIndex和matrics对应
+        # numColIndex matches matrics
         # g = GaugeMetricFamily('dbcops_' + self._matric, 'Return value of column ' + self._matric, self._label)
-        # 逐行获取对应列的值并采集到matrics
+        # fetch data from each row and collect with matric
         for row in cursor:
             tupLab = []
             for l in range(len(self._label)):
                 tupLab.append(row[labIndex[l]])
-            self._gauge.labels(*tupLab).set(row[2])
+            self._gauge.labels(*tupLab).set(row[valIndex])
             # g.add_metric(*tupLab, row[2])
             # g.set(row[numColIndex[i]])
 
     def do_execute(self):
-        # 建立连接
-        cx_Oracle.init_oracle_client(
-            lib_dir=self._client)
+        # build connection
         while True:
             with cx_Oracle.connect(self._database) as conn:
                 with conn.cursor() as cur:
